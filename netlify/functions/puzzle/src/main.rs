4db5c943578291635f757852987c68a347521cf4
Richard Hess <eswat2@gmail.com>
2022-11-04 12:42:30 -0700

    fix puzzle build issues

--- netlify/functions/puzzle/src/main.rs
+++ netlify/functions/puzzle/src/main.rs
@@ -3,6 +3,7 @@ extern crate serde_json;
 
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use aws_lambda_events::encodings::Body;
+use http::header;
 use http::header::HeaderMap;
 use lambda_runtime::{handler_fn, Context, Error};
 use log::LevelFilter;
@@ -19,7 +20,7 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
+pub(crate) async fn my_handler(_event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
   let start = Instant::now();
   let generated = Sudoku::generate_unique();
   let sudoku_line = generated.to_str_line();
@@ -53,12 +54,16 @@ pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> 
   });
 
   let payload = serde_json::to_string(&data).unwrap();
+    let mut head = HeaderMap::new();
+    let multi = HeaderMap::new();
+
+    head.insert(header::CONTENT_TYPE, "application/json".parse().unwrap());
 
     let resp = ApiGatewayProxyResponse {
         status_code: 200,
-        headers: HeaderMap::new({ "Content-Type", "application/json" }),
-        multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(format!(payload))),
+        headers: head,
+        multi_value_headers: multi,
+        body: Some(Body::Text(format!("{}", payload))),
         is_base64_encoded: Some(false),
     };
 
